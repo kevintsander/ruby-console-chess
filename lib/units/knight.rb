ab0f50befd9974c9552c511ba38c9b39f85c9634
Kevin Sander <kevintsander@gmail.com>
2022-08-16 14:27:34 -0400

    added knight initial setup

---
+++ lib/units/knight.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require './lib/unit'
+
+# Represents a King chess piece
+class Knight < Unit
+  @jump_move = true
+
+  def initialize(location, player, id = location)
+    super(location, player, id)
+    @allowed_move_deltas = { jump_standard: knight_deltas,
+                             jump_attack: knight_deltas }
+  end
+
+  private
+
+  def knight_deltas
+    [[2, 3], [2, -3], [-2, 3], [-2, -3], [3, 2], [3, -2], [-3, 2], [-3, -2]]
+  end
+end
