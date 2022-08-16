2760065cffd3d62e084a0fb24756b0a7db4b30f8
Kevin Sander <kevintsander@gmail.com>
2022-08-16 14:31:24 -0400

    added initial rook setup

---
+++ lib/units/rook.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require './lib/unit'
+
+# Represents a King chess piece
+class Rook < Unit
+  def initialize(location, player, id = location)
+    super(location, player, id)
+    @allowed_move_deltas = { move_standard: rook_deltas,
+                             move_attack: rook_deltas }
+  end
+
+  private
+
+  def rook_deltas
+    (1..7).reduce([]) { |all, dist| all + [[0, dist], [0, -dist], [dist, 0], [-dist, 0]] }
+  end
+end
