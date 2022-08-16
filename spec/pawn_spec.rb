27284c568bc3e41259d27471f1219b24b7059f9e
Kevin Sander <kevintsander@gmail.com>
2022-08-16 15:33:48 -0400

    added tests for pawn direction

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -3,6 +3,38 @@
 require './lib/units/pawn'
 
 describe Pawn do
+  context 'started in rank 7' do
+    let(:player) { double('player', name: 'player1', color: :black) }
+    subject(:pawn_seven) { described_class.new('g7', player) }
+    it 'can only attack or move to lower rank' do
+      move_ranks = pawn_seven.allowed_move_deltas[:move_standard].map { |item| item[0] }
+      attack_ranks = pawn_seven.allowed_move_deltas[:move_attack].map { |item| item[0] }
+      en_passant_ranks = pawn_seven.allowed_move_deltas[:en_passant].map { |item| item[0] }
+      expect(move_ranks).to include(-1)
+      expect(move_ranks).not_to include(1)
+      expect(attack_ranks).to include(-1)
+      expect(attack_ranks).not_to include(1)
+      expect(en_passant_ranks).to include(-1)
+      expect(en_passant_ranks).not_to include(1)
+    end
+  end
+
+  context 'started in rank 2' do
+    let(:player) { double('player', name: 'player1', color: :black) }
+    subject(:pawn_two) { described_class.new('g2', player) }
+    it 'can only attack or move to higher rank' do
+      move_ranks = pawn_two.allowed_move_deltas[:move_standard].map { |item| item[0] }
+      attack_ranks = pawn_two.allowed_move_deltas[:move_attack].map { |item| item[0] }
+      en_passant_ranks = pawn_two.allowed_move_deltas[:en_passant].map { |item| item[0] }
+      expect(move_ranks).to include(1)
+      expect(move_ranks).not_to include(-1)
+      expect(attack_ranks).to include(1)
+      expect(attack_ranks).not_to include(-1)
+      expect(en_passant_ranks).to include(1)
+      expect(en_passant_ranks).not_to include(-1)
+    end
+  end
+
   describe '#initialize' do
     context 'player color is black' do
       let(:black_player) { double('player', name: 'player1', color: :black) }
