652408f1a6fbfd87742625f0684c9fe3c969107c
Kevin Sander <kevintsander@gmail.com>
2022-08-16 14:14:00 -0400

    minor test corrections

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -6,19 +6,19 @@ describe Bishop do
   describe '#initialize' do
     context 'player color is black' do
       let(:black_player) { double('player', name: 'player1', color: :black) }
-      subject(:black_king) { described_class.new('c8', black_player) }
+      subject(:black_bishop) { described_class.new('c8', black_player) }
 
       it 'sets symbol to black bishop' do
-        expect(black_king.symbol).to eq('♝')
+        expect(black_bishop.symbol).to eq('♝')
       end
     end
 
     context 'player color is white' do
       let(:white_player) { double('player', name: 'player1', color: :white) }
-      subject(:white_king) { described_class.new('c1', white_player) }
+      subject(:white_bishop) { described_class.new('c1', white_player) }
 
       it 'sets symbol to white bishop' do
-        expect(white_king.symbol).to eq('♗')
+        expect(white_bishop.symbol).to eq('♗')
       end
     end
   end
--- spec/queen_spec.rb
+++ spec/queen_spec.rb
@@ -6,19 +6,19 @@ describe Queen do
   describe '#initialize' do
     context 'player color is black' do
       let(:black_player) { double('player', name: 'player1', color: :black) }
-      subject(:black_king) { described_class.new('d8', black_player) }
+      subject(:black_queen) { described_class.new('d8', black_player) }
 
       it 'sets symbol to black queen' do
-        expect(black_king.symbol).to eq('♛')
+        expect(black_queen.symbol).to eq('♛')
       end
     end
 
     context 'player color is white' do
       let(:white_player) { double('player', name: 'player1', color: :white) }
-      subject(:white_king) { described_class.new('d1', white_player) }
+      subject(:white_queen) { described_class.new('d1', white_player) }
 
       it 'sets symbol to white queen' do
-        expect(white_king.symbol).to eq('♕')
+        expect(white_queen.symbol).to eq('♕')
       end
     end
   end
