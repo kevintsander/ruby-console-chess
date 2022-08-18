f46ff2a78a8ea5d9e1d18f886fad9304998870d3
Kevin Sander <kevintsander@gmail.com>
2022-08-18 14:50:08 -0400

    added kingside, queenside castle moves

--- lib/units/king.rb
+++ lib/units/king.rb
@@ -7,7 +7,9 @@ class King < Unit
   def initialize(location, player, id = location)
     super(location, player, id)
     @allowed_actions_deltas = { move_standard: king_deltas,
-                                move_attack: king_deltas }
+                                move_attack: king_deltas,
+                                kingside_castle: kingside_castle_delta,
+                                queenside_castle: queenside_castle_delta }
   end
 
   private
@@ -16,5 +18,13 @@ class King < Unit
     straight = [[0, 1], [0, -1], [1, 0], [-1, 0]]
     diagonal = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
     straight + diagonal
+  end
+
+  def kingside_castle_delta
+    [0, 2]
+  end
+
+  def queenside_castle_delta
+    [0, -2]
   end
 end
--- lib/units/rook.rb
+++ lib/units/rook.rb
@@ -7,12 +7,22 @@ class Rook < Unit
   def initialize(location, player, id = location)
     super(location, player, id)
     @allowed_actions_deltas = { move_standard: rook_deltas,
-                                move_attack: rook_deltas }
+                                move_attack: rook_deltas,
+                                kingside_castle: kingside_castle_delta,
+                                queenside_castle: queenside_castle_delta }
   end
 
   private
 
   def rook_deltas
     (1..7).reduce([]) { |all, dist| all + [[0, dist], [0, -dist], [dist, 0], [-dist, 0]] }
+  end
+
+  def kingside_castle_delta
+    [0, -2]
+  end
+
+  def queenside_castle_delta
+    [0, 3]
   end
 end
