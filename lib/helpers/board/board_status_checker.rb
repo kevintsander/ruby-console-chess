b39dbe07bd0215c8ec95d1058e8c00c4ddd4d1b0
Kevin Sander <kevintsander@gmail.com>
2022-08-18 11:15:56 -0400

    added #enemy_can_attack_move?

--- lib/helpers/board/board_status_checker.rb
+++ lib/helpers/board/board_status_checker.rb
@@ -9,4 +9,16 @@ module BoardStatusChecker
     end
     false
   end
+
+  def enemy_can_attack_move?(unit, move_location)
+    check_location = unit.location
+    until check_location == move_location
+      check_location = step_location(check_location, move_location)
+
+      enemy_units(unit) do |enemy|
+        return true if allowed_locations(enemy).include?(check_location)
+      end
+    end
+    false
+  end
 end
